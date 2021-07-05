dd9475efd3a7cd2a6263e75f2509c737a925a383
Marcelo Pepis <61587462+marcelopepisatos@users.noreply.github.com>
2021-07-05 15:02:29 -0300

    limpando o app react e instalando dependências.

---
+++ calculando/src/components/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Nav, NavbarContainer, NavLogo } from './NavBarElements';
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        <NavbarContainer>
+          <NavLogo>
+            Calculando
+          </NavLogo>
+        </NavbarContainer>
+      </Nav>
+    </>
+  )
+}
+
+export default Navbar
