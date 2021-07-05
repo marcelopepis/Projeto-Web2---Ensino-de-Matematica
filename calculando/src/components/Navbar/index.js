a8ae22eb8aa0c9d31f14c07717a470438bc1d60b
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-07-05 16:24:09 -0300

    criando a navbar

--- calculando/src/components/Navbar/index.js
+++ calculando/src/components/Navbar/index.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Nav, NavbarContainer, NavLogo } from './NavBarElements';
+import React from 'react';
+import { FaBars } from 'react-icons/fa';
+import { Nav, NavbarContainer, NavLogo, MobileIcon, NavMenu, NavItem, NavLinks } from './NavBarElements';
 
 const Navbar = () => {
   return (
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo>
+          <NavLogo to='/'>
             Calculando
           </NavLogo>
+          <MobileIcon>
+            <FaBars />
+          </MobileIcon>
+          <NavMenu>
+            <NavItem>
+              <NavLinks to='about'>Sobre</NavLinks>
+            </NavItem>
+          </NavMenu>
         </NavbarContainer>
       </Nav>
     </>
   )
 }
 
-export default Navbar
+export default Navbar;
