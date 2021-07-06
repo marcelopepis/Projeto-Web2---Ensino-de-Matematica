dfa85022ceff9cc429b0fb095cf0f5d765ec23e3
Yohan D. Pessanha <42162269+yohandp@users.noreply.github.com>
2021-07-06 20:01:17 -0300

    atualizando

--- calculando/src/views/home/index.js
+++ calculando/src/views/home/index.js
@@ -16,6 +16,7 @@ import adicao from '../../images/adicao.png';
 import subtracao from '../../images/subtracao.png';
 import multiplicacao from '../../images/multiplicacao.png';
 import divisao from '../../images/divisao.png';
+import igual from '../../images/igual.png';
 
 const Home = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -24,6 +25,9 @@ const Home = () => {
     setIsOpen(!isOpen);
   }
 
+  var n1 = 0;
+  var n2;
+
   return (
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
@@ -31,23 +35,23 @@ const Home = () => {
       <div className="rep">
         <table>
           <tr>
-            <td><input type="image" src={um}  alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={dois} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={tres} alt="logo" className="center zoom"/></td>
+            <td><img src={um}  alt="logo" className="center zoom" id="um" onClick={n1 = 1} ></img></td>
+            <td><img src={dois} alt="logo" className="center zoom" id="dois" onClick={n1 = 2}/></td>
+            <td><img src={tres} alt="logo" className="center zoom" id="tres" onClick={n1 = 3}/></td>
           </tr>
           <tr>
-            <td><input type="image" src={quatro} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={cinco} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={seis} alt="logo" className="center zoom"/></td>
+            <td><img src={quatro} alt="logo" className="center zoom" id="quatro" onClick={n1 = 4}/></td>
+            <td><img src={cinco} alt="logo" className="center zoom" id="cinco" onClick={n1 = 5}/></td>
+            <td><img src={seis} alt="logo" className="center zoom" id="seis" onClick={n1 = 6}/></td>
           </tr>
           <tr>
-            <td><input type="image" src={sete} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={oito} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={nove} alt="logo" className="center zoom"/></td>
+            <td><img src={sete} alt="logo" className="center zoom" id="sete" onClick={n1 = 7}/></td>
+            <td><img src={oito} alt="logo" className="center zoom" id="oito" onClick={n1 = 8}/></td>
+            <td><img src={nove} alt="logo" className="center zoom" id="nove" onClick={n1 = 9}/></td>
           </tr>
           <tr>
             <td></td>
-            <td><input type="image" src={zero} alt="logo" className="center zoom"/></td>
+            <td><img src={zero} alt="logo" className="center zoom" id="zero" onClick={n1 = 0}/></td>
             <td></td>
           </tr>
         </table>
@@ -55,14 +59,17 @@ const Home = () => {
       <div className="rep2">
         <table>
           <tr>
-            <td><input type="image" src={adicao} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={subtracao} alt="logo" className="center zoom"/></td>
+            <td><img src={adicao} alt="logo" className="center zoom"/></td>
+            <td><img src={subtracao} alt="logo" className="center zoom"/></td>
           </tr>
           <tr> 
-            <td><input type="image" src={multiplicacao} alt="logo" className="center zoom"/></td>
-            <td><input type="image" src={divisao} alt="logo" className="center zoom"/></td>
+            <td><img src={multiplicacao} alt="logo" className="center zoom"/></td>
+            <td><img src={divisao} alt="logo" className="center zoom"/></td>
           </tr>
         </table>
+      </div>
+      <div>
+        <img src={igual} alt="logo" className="igual"></img>
       </div>
     </>
   )
