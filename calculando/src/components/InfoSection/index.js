c283fa30c7649b7ef5b57c96a646c6de505eab71
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-07-07 22:29:11 -0300

    resolvido problema com a imagem. Adicionado relatório inicial do projeto.

--- calculando/src/components/InfoSection/index.js
+++ calculando/src/components/InfoSection/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Button} from '../ButtonElement';
 import { InfoContainer, InfoRow, InfoWrapper, Column1, TextWrapper, TopLine, Heading, Subtitle, BtnWrap,
 Column2, ImgWrap, Img} from './InfoElements';
+import img1 from '../../images/svg-1.svg';
 
 const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkText, description, buttonLabel, img, alt, primary, dark}) => {
   return (
@@ -28,7 +29,7 @@ const InfoSection = ({lightBg, id, imgStart, topLine, lightText, headLine, darkT
             </Column1>
             <Column2>
               <ImgWrap>
-                <Img src={require('../../images/svg-1.svg')}/>
+                <Img src={img1} alt={alt}/>
               </ImgWrap>
             </Column2>
           </InfoRow>
