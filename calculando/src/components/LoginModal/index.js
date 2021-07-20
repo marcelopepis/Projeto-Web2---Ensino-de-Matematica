2e98a3c80ca8bd470ff738e873778483955bd7b6
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-07-20 18:56:50 -0300

    criando o modal de login

---
+++ calculando/src/components/LoginModal/index.js
@@ -0,0 +1,48 @@
+import {React, useState} from 'react'
+import Modal from 'react-modal';
+import { Container } from './styles';
+import {api} from '../../services/api';
+
+
+
+/*interface NewUserModalProps {
+  isOpen: Boolean;
+  onRequestClose: () => void;
+}*/
+
+
+const LoginModal = ({isOpen, onRequestClose}) => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+
+  async function handleLogin(event) {
+    event.preventDefault();
+    const response = await api.post('/login', {
+      email: email,
+      password: password
+    })
+    const {_id} = response.data
+    console.log(_id);    
+  }
+  return(
+    <>
+      <Modal isOpen ={isOpen}
+       onRequestClose={onRequestClose}
+       overlayClassName="react-modal-overlay"
+       className="react-modal-content"
+       ariaHideApp={false}
+      >
+        <Container onSubmit={handleLogin}>
+          <h2>Entrar</h2>
+          <input value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="E-mail"></input>
+          <input value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Sua Senha"></input>  
+          <button type="submit">Login</button>
+        </Container>
+      </Modal>
+    </>
+  );
+};
+
+export default LoginModal;
---
+++ calculando/src/components/LoginModal/styles.js
@@ -0,0 +1,47 @@
+import styled from 'styled-components';
+
+
+export const Container = styled.form`
+  h2 {
+    color: #363F5F;
+    font-size: 1.5rem;
+    margin-bottom: 2rem;
+  }
+
+  input {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    border-radius: 0%.25rem;
+    border: 1px solid #d7d7d7;
+    background: #e7e9ee;
+    font-weight: 400;
+    font-size: 1rem;
+
+    &::placeholder {
+      color: #969CB3;
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+    width: 100%;
+    padding: 0 1.5rem;
+    height: 4rem;
+    background: #2ca4f8;
+    color: #fff;
+    border-radius: 0.25rem;
+    border: 0;
+    font-size: 1rem;
+    margin-top: 1.5rem;
+    transition: filter 0.2s;
+    font-weight: 600;
+
+    &:hover {
+      filter: brightness(0.9);
+    }
+  }
+`;
