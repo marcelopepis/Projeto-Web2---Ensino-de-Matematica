281e8bd3b1f9ad6b809bdb85e2f4cf722fda86ed
Yohan <yohandpessanha@gmail.com>
2021-07-26 18:43:12 -0300

    professor em cadastro mas falta aplicar valor

--- calculando/src/pages/index.js
+++ calculando/src/pages/index.js
@@ -3,7 +3,6 @@ import Sidebar from '../components/Sidebar';
 import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import InfoSection from '../components/InfoSection';
-import EmailModal from '../components/EmailModal';
 import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour, homeObjFive } from '../components/InfoSection/Data';
 
 const Home = () => {
@@ -12,17 +11,7 @@ const Home = () => {
   const toggle = () => {
     setIsOpen(!isOpen);
   }
-
-  const [isEmailModalOpen, setisEmailModalOpen] = useState(false);
-
-  function handleOpenEmailModal() {
-    setisEmailModalOpen(true);
-  };
-
-  function handleCloseEmailModalOpen() {
-    setisEmailModalOpen(false);
-  };
-
+  
   return (
     <>
       <Sidebar isOpen={isOpen} toggle={toggle} />
