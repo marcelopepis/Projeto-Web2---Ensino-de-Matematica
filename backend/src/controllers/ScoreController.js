7f86e79f6ab0e39c2881c283852a44d10cf45f03
Marcelo Pepis <marcelo.pepis@gmail.com>
2021-08-09 18:18:29 -0300

    Criadas as rotas para update de número de respostas certas e erradas.

---
+++ backend/src/controllers/ScoreController.js
@@ -0,0 +1,21 @@
+const User = require('../models/User');
+
+
+
+module.exports = {
+  async updateRight(req, res) {
+    const _id = req.body.id_user;
+
+    let user = await User.findOneAndUpdate({_id}, {$inc : {'right_answers' : 1}});
+
+    return res.json({right_answers: user.right_answers});
+  },
+
+  async updateWrong(req, res) {
+    const _id = req.body.id_user;
+
+    let user = await User.findOneAndUpdate({_id}, {$inc : {'right_answers' : 1}});
+
+    return res.json({wrong_answers: user.wrong_answers});
+  }
+}
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
 const ClassesController = require('./controllers/ClassesController');
+const ScoreController = require('./controllers/ScoreController');
 
 const routes = express.Router();
 
@@ -8,6 +9,8 @@ routes.post('/users', SessionController.store);
 routes.post('/login', SessionController.login);
 routes.post('/classes', ClassesController.store);
 routes.get('/listClasses', ClassesController.listClasses);
-routes.get('/listClassMembers', ClassesController.listClassMembers);
+routes.get('/listclassmembers', ClassesController.listClassMembers);
+routes.post('/updateright', ScoreController.updateRight);
+routes.post('/updatewrong', ScoreController.updateWrong);
 
 module.exports = routes;
